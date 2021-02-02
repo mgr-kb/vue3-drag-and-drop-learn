903caa1d6b3ac7c63e70712cb83e00993281349d
mgr <koumgrars13@gmail.com>
2021-02-02 23:17:33 +0900

    drag & drop処理実装中

--- src/components/TaskColumn.vue
+++ src/components/TaskColumn.vue
@@ -2,20 +2,48 @@
   <div class="bg-gray-200 rounded-lg column-width px-2 py-4 mr-2">
     <p>{{ title }}</p>
     <!-- task container -->
-    <div>
-      <div class="mt-2 bg-blue-400" draggable>taskA</div>
+    <ul>
+      <li
+        v-for="item in data"
+        :key="item.id"
+        class="mt-2 bg-blue-400"
+        draggable="true"
+        @dragstart="dragList($event, item.id)"
+      >
+        {{ item.name }}
+      </li>
+      <!-- <div class="mt-2 bg-blue-400" draggable>taskA</div>
       <div class="mt-2 bg-blue-400" draggable>taskB</div>
-      <div class="mt-2 bg-blue-400" draggable>taskC</div>
-    </div>
+      <div class="mt-2 bg-blue-400" draggable>taskC</div> -->
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive } from "vue";
 
 export default defineComponent({
   props: {
     title: String
+  },
+  setup() {
+    const data = reactive([
+      { id: "1", name: "TaskA", status: "Todo" },
+      { id: "2", name: "TaskB", status: "In Progree" },
+      { id: "3", name: "TaskC", status: "Done" }
+    ]);
+    // drag開始時のイベント処理
+    const dragList = (event: DragEvent, taskId: string) => {
+      console.log(taskId);
+      // dataTransferは必ず存在すると仮定
+      event.dataTransfer!.effectAllowed = "move";
+      event.dataTransfer!.dropEffect = "move";
+      event.dataTransfer!.setData("list-id", taskId);
+    };
+    return {
+      data,
+      dragList
+    };
   }
 });
 </script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,24 +3,132 @@
     <h1 class="text-indigo-500">{{ msg }}</h1>
     <div class="flex justify-center">
       <div class="min-h-screen flex overflow-x-scroll py-12">
-        <TaskColumn title="Todo" />
+        <!-- TODO -->
+        <div
+          class="bg-gray-200 rounded-lg column-width px-2 py-4 mr-2"
+          @drop="dropList($event, 'Todo')"
+        >
+          <p>TODO</p>
+          <!-- task container -->
+          <ul>
+            <li
+              v-for="item in statusIsTodo"
+              :key="item.id"
+              class="mt-2 bg-blue-400"
+              draggable="true"
+              @dragstart="dragList($event, item.id)"
+            >
+              {{ item.name }}
+            </li>
+          </ul>
+        </div>
+        <!-- In Progress -->
+        <div
+          class="bg-gray-200 rounded-lg column-width px-2 py-4 mr-2"
+          @drop="dropList($event, 'InProgress')"
+        >
+          <p>In Progress</p>
+          <!-- task container -->
+          <ul>
+            <li
+              v-for="item in statusIsProgress"
+              :key="item.id"
+              class="mt-2 bg-blue-400"
+              draggable="true"
+              @dragstart="dragList($event, item.id)"
+            >
+              {{ item.name }}
+            </li>
+          </ul>
+        </div>
+        <!-- Done -->
+        <div
+          class="bg-gray-200 rounded-lg column-width px-2 py-4 mr-2"
+          @drop="dropList($event, 'Done')"
+        >
+          <p>Done</p>
+          <!-- task container -->
+          <ul>
+            <li
+              v-for="item in statusIsDone"
+              :key="item.id"
+              class="mt-2 bg-blue-400"
+              draggable="true"
+              @dragstart="dragList($event, item.id)"
+            >
+              {{ item.name }}
+            </li>
+          </ul>
+        </div>
+        <!-- <TaskColumn title="Todo" />
         <TaskColumn title="In Progress" />
-        <TaskColumn title="Done" />
+        <TaskColumn title="Done" /> -->
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import TaskColumn from "@/components/TaskColumn.vue";
+import { defineComponent, reactive, computed } from "vue";
+// import TaskColumn from "@/components/TaskColumn.vue";
 
+/**
+ * dataTransferなど、nullableな値も必ずあると仮定
+ */
 export default defineComponent({
-  components: {
-    TaskColumn
-  },
+  // components: {
+  //   TaskColumn
+  // },
   props: {
     msg: String
+  },
+  setup() {
+    const data = reactive([
+      { id: "1", name: "TaskA", status: "Todo" },
+      { id: "2", name: "TaskB", status: "InProgress" },
+      { id: "3", name: "TaskC", status: "Done" },
+      { id: "4", name: "TaskD", status: "Todo" },
+      { id: "5", name: "TaskE", status: "InProgress" },
+      { id: "6", name: "TaskF", status: "Done" },
+      { id: "7", name: "TaskG", status: "Todo" },
+      { id: "8", name: "TaskH", status: "InProgress" },
+      { id: "9", name: "TaskI", status: "Done" }
+    ]);
+    const statusIsTodo = computed(() =>
+      data.filter(item => item.status === "Todo")
+    );
+    const statusIsProgress = computed(() =>
+      data.filter(item => item.status === "InProgress")
+    );
+    const statusIsDone = computed(() =>
+      data.filter(item => item.status === "Done")
+    );
+    // drag開始時のイベント処理
+    const dragList = (event: DragEvent, taskId: string) => {
+      console.log(taskId);
+      event.dataTransfer!.effectAllowed = "move";
+      event.dataTransfer!.dropEffect = "move";
+      event.dataTransfer!.setData("list-id", taskId);
+    };
+    // drop時のイベント処理
+    const dropList = (event: DragEvent, status: string) => {
+      const dragId = event.dataTransfer!.getData("list-id");
+      const dragItem = data.find(item => item.id === dragId);
+      dragItem!.status = status;
+    };
+    return {
+      statusIsTodo,
+      statusIsProgress,
+      statusIsDone,
+      dragList
+    };
   }
 });
 </script>
+
+<style scoped>
+.column-width {
+  min-width: 320px;
+  width: 320px;
+}
+</style>
